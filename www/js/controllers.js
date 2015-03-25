1c1b8daf2ebbda7648e401dda0f3d11136d94398
Odín del Río <odin.delrio@atrapalo.com>
2015-03-25 21:08:18 +0100

    Formatting and fix names.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,50 +1,51 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-  // Form data for the login modal
-  $scope.loginData = {};
+    .controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
+        // Form data for the login modal
+        $scope.loginData = {};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
-
-.controller('PostsCtrl', function($scope, $http) {
-
-    $http.get('http://elpatin.com/wp-json/posts')
-        .success(function(posts) {
-            $scope.posts = posts;
+        // Create the login modal that we will use later
+        $ionicModal.fromTemplateUrl('templates/login.html', {
+            scope: $scope
+        }).then(function (modal) {
+            $scope.modal = modal;
         });
-})
 
-.controller('PostCtrl', function($scope, $http, $stateParams) {
+        // Triggered in the login modal to close it
+        $scope.closeLogin = function () {
+            $scope.modal.hide();
+        };
+
+        // Open the login modal
+        $scope.login = function () {
+            $scope.modal.show();
+        };
+
+        // Perform the login action when the user submits the login form
+        $scope.doLogin = function () {
+            console.log('Doing login', $scope.loginData);
+
+            // Simulate a login delay. Remove this and replace with your login
+            // code if using a login system
+            $timeout(function () {
+                $scope.closeLogin();
+            }, 1000);
+        };
+    })
+
+    .controller('PostsCtrl', function ($scope, $http) {
+
+        $http.get('http://elpatin.com/wp-json/posts')
+            .success(function (posts) {
+                $scope.posts = posts;
+            });
+    })
+
+    .controller('PostCtrl', function ($scope, $sce, $http, $stateParams) {
 
         $http.get('http://elpatin.com/wp-json/posts/' + $stateParams.postId)
-            .success(function(post) {
+            .success(function (post) {
+                $scope.postContent = $sce.trustAsHtml(post.content);
                 $scope.post = post;
             });
 
